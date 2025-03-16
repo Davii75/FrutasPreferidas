6ff441371079f48f0ecfa16f2862daf7a2f136ac
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-16 13:41:13 -0300

    Finaliza programa que mostra minhas frutas favoritas.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("--- Minhas frutas preferidas ---");
+Console.WriteLine();
+Console.WriteLine("1. Pera");
+Console.WriteLine("2. Tangerina");
+Console.WriteLine("3. Manga");
+Console.WriteLine("4. Maçã");
+Console.WriteLine("5. Abacaxi");
